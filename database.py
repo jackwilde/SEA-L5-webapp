34a0cb55a217d866211f71644b22dd22f59e82ca
Jack Wilde <jack.wilde@uwe.ac.uk>
2023-10-31 14:23:48 +0000

    Update db path

--- database.py
+++ database.py
@@ -1,6 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
+from pathlib import Path
 
+
+DB_NAME = "data.db"
+DB_PATH = f"{Path(__file__).parent}/{DB_NAME}"
 db = SQLAlchemy()
 
 # Create database
-SQLALCHEMY_DATABASE_URI = "sqlite:///data.db"
+SQLALCHEMY_DATABASE_URI = f"sqlite:///{DB_PATH}"
